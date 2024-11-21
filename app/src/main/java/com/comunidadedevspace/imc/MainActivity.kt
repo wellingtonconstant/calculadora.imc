e94ef4e10a9edef55afe30b764cfc454c5a506da
Wellington Constant <wellington.constant1@gmail.com>
2024-11-21 11:45:38 -0300

    Initial commit

---
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.comunidadedevspace.imc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btnCalular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalular.setOnClickListener {
+
+
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+                Snackbar
+                    .make(
+                        edtPeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
+            }
+        }
+    }
+}
